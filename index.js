addde0300e090ca2784ff7e003e61fb1e2335288
Gabe O'Brien <gabe.obrien@lacework.net>
2025-02-25 11:33:32 -0800

    feat: fist pass at simple secure hello world web app

---
+++ index.js
@@ -0,0 +1,11 @@
+const mod = require('hello-mod');
+const express = require('express');
+const port = 3000;
+const app = express();
+
+mod.init(app, __dirname);
+app.get('/', mod.manageIndexRoute)
+
+app.listen(port, () => {
+ console.log(`Server is running at http://localhost:${port}`);
+});
