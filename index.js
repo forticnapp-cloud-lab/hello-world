92ab8dba7be8369b8b9a598cb475152c5731c96e
Gabe O'Brien <gabe.obrien@lacework.net>
2025-02-28 17:01:53 -0800

    fix: moves JS to own file

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 
 mod.init(app, __dirname);
 app.get('/', mod.manageIndexRoute)
+app.use(express.static('public'))
 
 app.listen(port, () => {
  console.log(`Server is running at http://localhost:${port}`);
