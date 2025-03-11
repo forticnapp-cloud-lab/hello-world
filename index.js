9f1c73103d5920b51111a8ccb83c0303b1bc99c3
Gabe O'Brien <gabe.obrien@lacework.net>
2025-03-11 14:39:58 -0700

    feat: updates to make editing the hello world text easier

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const mod = require('hello-mod');
 const express = require('express');
+const es6Renderer = require('express-es6-template-engine')
 const port = 3000;
 const app = express();
+
+app.engine('html', es6Renderer);
+app.set('views', 'views');
+app.set('view engine', 'html');
 
 mod.init(app, __dirname);
 app.get('/', mod.manageIndexRoute)
 app.use(express.static('public'))
+
+global.hello_message = "Hello World!"
 
 app.listen(port, () => {
  console.log(`Server is running at http://localhost:${port}`);
--- public/js/sprinkle.js
+++ public/js/sprinkle.js
@@ -1,25 +1,26 @@
-// do this for 30 seconds
-var duration = 30 * 1000;
-var end = Date.now() + duration;
+function launchit(){
+  var duration = 25 * 1000;
+  var end = Date.now() + duration;
 
-(function frame() {
-  // launch a few confetti from the left edge
-  confetti({
-    particleCount: 7,
-    angle: 60,
-    spread: 100,
-    origin: { x: 0 }
-  });
-  // and launch a few from the right edge
-  confetti({
-    particleCount: 7,
-    angle: 120,
-    spread: 100,
-    origin: { x: 1 }
-  });
+  (function frame() {
+    // launch a few confetti from the left edge
+    confetti({
+      particleCount: 7,
+      angle: 60,
+      spread: 100,
+      origin: { x: 0 }
+    });
+    // and launch a few from the right edge
+    confetti({
+      particleCount: 7,
+      angle: 120,
+      spread: 100,
+      origin: { x: 1 }
+    });
 
-  // keep going until we are out of time
-  if (Date.now() < end) {
-    requestAnimationFrame(frame);
-  }
-}());
+    // keep going until we are out of time
+    if (Date.now() < end) {
+      requestAnimationFrame(frame);
+    }
+  }());
+}
