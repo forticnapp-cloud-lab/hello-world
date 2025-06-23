94db25adc0456396c5fe3b2dce102a6cd49e6bc7
Gabe O'Brien <gabe.obrien@lacework.net>
2025-06-23 15:38:43 -0700

    feat: switch to handlebars template engine

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const mod = require('hello-mod');
 const express = require('express');
 const es6Renderer = require('express-es6-template-engine')
+const handlebars = require('express-handlebars')
 const port = 3000;
 const app = express();
 
-app.engine('html', es6Renderer);
-app.set('views', 'views');
-app.set('view engine', 'html');
-
-mod.init(app, __dirname);
-app.get('/', mod.manageIndexRoute)
+app.set('view engine', 'hbs');
+app.engine('hbs', handlebars({
+  layoutsDir: __dirname + '/views/layouts',
+  extname: "hbs"
+}))
 app.use(express.static('public'))
 
 global.hello_message = "Hello World!"
 global.image_url     = ""
+
+app.get('/', mod.manageIndexRoute)
 
 app.listen(port, () => {
  console.log(`Server is running at http://localhost:${port}`);
